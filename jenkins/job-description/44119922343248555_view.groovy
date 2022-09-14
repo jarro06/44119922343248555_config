d5d67b0887386a3431945e6b93ae75d9d15552dc
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-09-14 13:01:53 +0200

    Add ADP toolchain configuration template

---
+++ jenkins/job-description/44119922343248555_view.groovy
@@ -0,0 +1,16 @@
+ 
+listView('44119922343248555 Jobs') {
+    description('44119922343248555 Jobs')
+    jobs {
+        regex('44119922343248555_.+')
+    }
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+}
